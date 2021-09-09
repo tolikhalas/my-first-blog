8ea3cdbfd41a428f115098ff1a138c41a2086152
Anatolii <tolikgalas@gmail.com>
2021-09-09 09:53:02 +0300

    Urls change

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6-76+*@6(o)57q*mc#yn!zz-1(mdvy3dd2)^7dl&fx7!+x@ewo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['tolikhalas.pythonanywhere.com']
 
